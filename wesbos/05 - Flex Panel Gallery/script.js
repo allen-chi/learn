f16dcc219d2a23f6a3b84a722ffc596627aaa2a0
allen-chi <58610600+allen-chi@users.noreply.github.com>
2020-09-26 22:25:23 -0700

    Panels close when next is picked
    Panels will automatically close when a new one is selected

--- wesbos/05 - Flex Panel Gallery/script.js
+++ wesbos/05 - Flex Panel Gallery/script.js
@@ -1,15 +1,36 @@
 // console.log("hello world");
 
+// pulls a node list of all .panel classes and stores into panels
 const panels = document.querySelectorAll('.panel');
 
+// function to add/remove the open and open-active subclasses
+// trying to get it to also remove the subclasses when a different panel is selected
 function toggleOpen() {
-  console.log('Hello');
-  this.classList.toggle('open');
-  this.classList.toggle('open-active');
-}
-function logSomething() {
-    console.log("This is a test");
-};
+// check to see if the subclasses exist, and remove if they do
+panelsArray.forEach(panel => {
+    if (panel.classList.contains('open') && panel.classList.contains('open-active')) {
+        panel.classList.remove('open');
+        panel.classList.remove('open-active');
+    }
+      
+// add subclasses to open the selected tab
+    console.log('Hello');
+    this.classList.add('open');
+    this.classList.add('open-active');
+    console.log(this.classList);
 
+});
+}
+
+// takes the nodelist and converts it into an array
 panelsArray = Array.from(panels);
-panelsArray.forEach(panel => panel.addEventListener('click', toggleOpen));
+
+// iterates through the array and runs toggleOpen on the panel that is clicked
+    panelsArray.forEach(panel => {
+        console.log(panel.classList);
+        panel.classList.remove('open');
+        panel.classList.remove('open-active');
+    
+        panel.addEventListener('click', toggleOpen);
+    });
+
